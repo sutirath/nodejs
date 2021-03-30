2bb5b53f569799f6d158ef8cdb81d3fb4e0a2e16
sutirath <sutirathbas@gmail.com>
2021-03-30 22:32:16 +0700

    new

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  let data = "fllzQe5PDKwW1yrSZ2ad1Nc51h5pXhlH:UevuK51oBulAEQfU";
+  let buff = new Buffer(data);
+  let base64data = buff.toString("base64");
+  res.json(base64data);
+});
+
+app.post("/api/payment/confirm", (req, res) => {
+  
+  res.json(req);
+});
+
+app.listen(3000, () => {
+  console.log("Start server at port 3000.");
+});
